d84320d304bc6f94dbe15e70fb8524a213ff9e63
Rob2n <robin.leclerc@epitech.eu>
2020-01-22 16:36:31 +0100

    feat(spleeter): Spleeter operational + output listing (spleeter not working for the moment)

--- server/app.py
+++ server/app.py
@@ -1,11 +1,11 @@
 import os
-from flask import Flask, flash, request, redirect, url_for, send_from_directory
+from flask import Flask, flash, request, redirect, url_for, send_from_directory, render_template, send_file
 from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 
 UPLOAD_FOLDER = '.'
-ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+ALLOWED_EXTENSIONS = {'wav', 'mp3'}
 
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
@@ -16,7 +16,21 @@ def allowed_file(filename):
 
 @app.route('/uploads/<filename>')
 def uploaded_file(filename):
-    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+	# os.system("spleeter separate -i " + filename + " -p spleeter:2stems -o output")
+	return send_from_directory("./output", filename)
+	# return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+
+@app.route('/<path:req_path>')
+def dir_listing(req_path):
+	BASE_DIR = '.'
+	# Joining the base and the requested path
+	abs_path = os.path.join(BASE_DIR, req_path)
+	# Check if path is a file and serve
+	if os.path.isfile(abs_path):
+		return send_file(abs_path)
+	# Show directory contents
+	files = os.listdir(abs_path)
+	return render_template('index.html', files=files)
 
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
